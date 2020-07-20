5ff7035aa105011494ae41f502b8852e96916ede
prasooncheenan <prasooncc@gmail.com>
2020-07-20 15:33:07 +0530

    Update paramchecker.cpp
    split func. single responsibilty

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,12 +1,33 @@
-#include "paramchecker.h"
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  //int i;
+isrespRateOk(#include "paramchecker.h"
+
+bool isBPMOk(float bpm)
+{
   if(bpm < 70 || bpm > 150) {
     return false;
-  } else if(spo2 < 80) {
+  } else
+    return true;
+}
+
+bool isspo2Ok(float spo2)
+{
+  if(spo2 < 80) {
     return false;
-  } else if(respRate < 30 || respRate > 60) {
+  } else
+    return true;
+}
+
+bool isrespRateOk(float resprate)
+{
+  if(respRate < 30 || respRate > 60) {
     return false;
-  }
+  } else
   return true;
+}
+  
+bool vitalsAreOk(float bpm, float spo2, float respRate) {
+  //int i;
+    if(isBPMOk(bpm) && isspo2Ok(spo2) && isrespRateOk(respRate))
+      return true;
+    else 
+      return false;
 }
